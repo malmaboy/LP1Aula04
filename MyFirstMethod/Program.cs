27381edc6ca3265394fbffdee7d4c757f7994a50
Hugo Carvalho <huguinho66@live.com>
2020-04-02 09:34:19 +0100

    My First exercixe

---
+++ MyFirstMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFirstMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountTo10();
+            CountTo10();
+        }
+
+        static void CountTo10()
+        {
+            for (int i = 1; i <=10;i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
