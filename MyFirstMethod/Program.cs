32153037be9f506814724f7dad7a439c2416c61a
Hugo Carvalho <huguinho66@live.com>
2020-04-02 10:46:34 +0100

    Exercise 4, comentado

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,8 +2,16 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// this is our project's main class.
+    /// </summary>
     class Program
     {
+
+    /// <summary>
+    /// This is the main method, the program starts here.
+    /// </summary>
+    /// <param name="args">Not used in this program.</param>
         static void Main(string[] args)
         {
             CountTon(3);
@@ -11,12 +19,18 @@ namespace MyFirstMethod
             CountTo10();
         }
           
-
+        /// <summary>
+        /// This method will print numbers from 1 to 10.
+        /// </summary>
         static void CountTo10()
         {
             CountTon(10);
         }
 
+        /// <summary>
+        /// This methot will print numbers from 1 to <see cref = "n"/>
+        /// </summary>
+        /// <param name="n"></param>
         static void CountTon(int n)
         {
             for (int i = 1; i <=n;i++)
